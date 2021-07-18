24c89b412d141ac70960a9b0b98af5e4d9ed7c4d
fangguangyang <fangguangyang@live.com>
2021-07-18 23:10:21 +0800

    init

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-sudo yum install zsh -y
-cp ./.zshrc ~/
-cp -r ./.oh-my-zsh ~/
-sudo chsh -s /bin/zsh
+ZSH_CUSTOM=./.oh-my-zsh/custom
+git clone git://github.com/robbyrussell/oh-my-zsh.git ./.oh-my-zsh
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+#sudo yum install zsh -y
+#cp ./.zshrc ~/
+#cp -r ./.oh-my-zsh ~/
+#sudo chsh -s /bin/zsh
